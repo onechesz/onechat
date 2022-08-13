7971f81af12642d669f42106e816fca926cc37b2
Ivan Minyaev <minyaew.to@gmail.com>
2022-08-13 16:24:32 +0300

    Added settings

---
+++ onechat_project/onechat_project/local_settings.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = 'django-insecure-m0vnatc5krz^bj5g456_s92z=-4r9t0vsqfzcd+y$8^ay-talv'
+DEBUG = True
+ALLOWED_HOSTS = []
+DATABASES = {'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
+}}
