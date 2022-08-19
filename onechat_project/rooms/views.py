246087785971187c3800b2fd5a2e53f50e1908d4
Ivan Minyaev <minyaew.to@gmail.com>
2022-08-19 17:47:34 +0300

    Добавил различия цветов в сообщениях

--- onechat_project/rooms/views.py
+++ onechat_project/rooms/views.py
@@ -15,8 +15,10 @@ def rooms(request):
 def room(request, slug):
     room_info = Room.objects.get(slug=slug)
     messages = Message.objects.filter(room=room_info).order_by('-date_added')[:20][::-1]
+    current_user = request.user
 
     return render(request, 'rooms/room.html', {
         'room_info': room_info,
-        'messages': messages
+        'messages': messages,
+        'current_user': current_user
     })
