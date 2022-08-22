26c5af13911c3f96507feb29573ec1ae0bcbc658
Ivan Minyaev <minyaew.to@gmail.com>
2022-08-22 21:12:52 +0300

    Удалил ненужные файлы

--- onechat_project/rooms/routing.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import consumers as rooms_consumers
-from ..user import consumers as user_consumers
-
-websocket_urlpatterns = [
-    path('ws/<str:room_name>/', rooms_consumers.ChatConsumer.as_asgi()),
-    path('ws/<str:sender>-<str:receiver>/', user_consumers.ChatConsumer.as_asgi())
-]
